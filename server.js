ade3d9d3a926cf4469ce87cdcf8ee95eb1571cdc
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-29 16:44:58 -0500

    added a listen() methods to listen for incoming connections to port

--- server.js
+++ server.js
@@ -21,3 +21,7 @@ app.get("*", (req, res) =>
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
