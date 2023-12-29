fae1c4eb710c6369ac8316dfd1d4f46d787582df
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-29 16:42:48 -0500

    added HTML routes

--- server.js
+++ server.js
@@ -8,4 +8,16 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
