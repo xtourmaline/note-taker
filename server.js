6dc8c1612a0837d6dbfeefff4556a81d9699e812
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-29 17:15:41 -0500

    fixed HTML route
    deleted an HTML route and fixed bug that causes /notes to not go to the correct html file

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // listens for incoming connections to port
