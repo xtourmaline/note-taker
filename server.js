9d17c89abbb2b2953383e77559b2280ab7786b83
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-29 16:24:44 -0500

    sets up data parsing

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const fs = require("fs");
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
