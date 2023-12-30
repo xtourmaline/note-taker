6879fec7220f177ad10e89d3226c2e5142af6173
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-29 21:40:03 -0500

    worked on being able to save the note

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const path = require('path');
-const db = require("./db/db.json");
+const dbPath = path.join(__dirname, "/db/db.json");
 
 const app = express();
 const PORT = 3001;
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -19,18 +17,40 @@ app.get("/notes", (req, res) =>
 );
 
 app.get('/api/notes', (req, res) =>
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    fs.readFile(dbPath, (err, data) => {
         if (err) {
-            console.log("error");
+            console.log("Error reading JSON file.");
             return;
         }
+
         res.json(JSON.parse(data));
     })
 );
 
-// app.post('/api/notes', (req, res) =>
+app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    note.id = uuidv4();
 
-// );
+    fs.readFile(dbPath, (err, data) => {
+        if (err) {
+            console.log("Error reading JSON file.");
+            return;
+        }
+
+        const db = JSON.parse(data);
+        db.push(note);
+
+        fs.writeFile(dbPath, JSON.stringify(db), (err) => {
+            if (err) {
+                console.log("Error in saving database", err);
+            }
+            else {
+                console.log("Saved to database!");
+                res.status(200).json(note);
+            }
+        });
+    });
+});
 
 app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/index.html"))
